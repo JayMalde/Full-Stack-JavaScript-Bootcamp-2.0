9d7f7b82055bdf8781a6881f118545c4fd815f47
Jay Malde <maldejay77@gmail.com>
2023-07-05 23:06:48 +0530

    21-01-2023 JS - DOM1

---
+++ LectureWiseNotesAndFiles/27-01-2023 Dom Part 1/index.js
@@ -0,0 +1,11 @@
+// https://patatap.com/
+// https://neal.fun/
+
+// Get all H3 innerText from the wikipedia page
+// - document.querySelector("h3"); - greedy selector selects first
+// - document.querySelectorAll("h3"); - gives all but in a Nodelist
+
+// 1st Way Array.from(allH3);
+// 2nd Way [...allH3].map(item => item.innerText) [...] Spread Operator
+
+// Assignment - Create a hosted web page where wherever the user clicks creates circle/square
