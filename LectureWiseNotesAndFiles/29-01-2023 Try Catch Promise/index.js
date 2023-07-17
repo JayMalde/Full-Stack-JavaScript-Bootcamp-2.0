7546cedc223c347285ec005e881e46d84af1057e
Jay Malde <maldejay77@gmail.com>
2023-07-17 20:25:10 +0530

    29-01-2023 HOF

---
+++ LectureWiseNotesAndFiles/29-01-2023 Try Catch Promise/index.js
@@ -0,0 +1,14 @@
+// HOF & Callback
+// Rest & Spread
+// Try Catch
+// Promises and at the end a small MCQ Games
+
+// Spread
+// Sending data ...spread
+// Using data ...rest
+
+function sum1(x,y){
+    return x+y;
+}
+let vari = [5,7];
+console.log(sum1(...vari)); // Spread
