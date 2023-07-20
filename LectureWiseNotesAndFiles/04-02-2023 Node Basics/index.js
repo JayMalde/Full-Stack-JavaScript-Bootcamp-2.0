cef82cfa8b29e741889fe174e84fe55cd6ca9eb3
Jay Malde <maldejay77@gmail.com>
2023-07-20 21:42:59 +0530

    04-02-2023 JS Recap

---
+++ LectureWiseNotesAndFiles/04-02-2023 Node Basics/index.js
@@ -0,0 +1,26 @@
+// Node History
+// Creator - Ryan Dahl
+
+// How JS executes the code.
+// Why var is a global variable & let,const are not.
+// Call Stack
+// Lexical Scope
+// Scope Chaining
+
+// Data Type - Primitive & Non -Primitive
+// Null- defined by user Undefined- defined by system
+// Boolean - George Boole
+// Primitive => Single Value -> Number, String, Boolean, Null
+// Non-Primitive => Multiple Value -> Array, Objects
+
+// use strict
+// Template Literal - Interpolation - Template String
+// typeof
+
+// Operators
+
+// Conditionals Switch Case
+// Ternary Operator
+
+// Arrays - 2 ways to declare arrays
+// 
