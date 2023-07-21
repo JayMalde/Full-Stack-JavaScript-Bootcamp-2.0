34f511e78a3b4a72819aafa6da4cbe36dd9fb1bd
Jay Malde <maldejay77@gmail.com>
2023-07-21 21:03:25 +0530

    05-02-2023 JS Advanced Concepts

---
+++ LectureWiseNotesAndFiles/05-02-2023 JS Advanced/index.js
@@ -0,0 +1,18 @@
+// Var-Global & Let,const - Local
+// Hoisting
+// Execution Context
+// Call Stack
+// Closure
+
+// Hoisting
+add(1,2);
+sub(3,4);
+
+add(var1,var2)
+{
+    return var1+var2;
+}
+sub(var1,var2)
+{
+    return var1-var2;
+}
